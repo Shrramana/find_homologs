f4c04b0659350a4e1be175f7b618a810ef50e522
shrramana <shrramanacbe@gmail.com>
2023-09-09 02:28:43 -0400

    Commit for adding tblastn query

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,3 +4,5 @@ protein_query="$1"
 nucleotide_subject="$2"
 output_file="$3"
 
+tblastn -query "$protein_query" -subject "$nucleotide_subject" -outfmt "6 qseqid sseqid pident length" > blast_results.txt
+
