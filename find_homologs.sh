3a825d98fdeaa8538bf8406f8052d51acc425683
shrramana <shrramanacbe@gmail.com>
2023-09-13 22:56:49 -0400

    finalised script

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,19 +1,14 @@
 cd /home/shrramana/biol7200/class3/ex3/
 
-protein_query="$1"
-nucleotide_subject="$2"
+protein_q="$1"
+nucl_sub="$2"
 output_file="$3"
 
-tblastn -query "$protein_query" -subject "$nucleotide_subject" -outfmt "6 qseqid sseqid pident length" > blast_results.txt
+tblastn -query "$protein_q" -subject "$nucl_sub" -outfmt "6 qseqid sseqid pident length qlen" > blast_results.txt
 
-# Filter hits to keep only those with >30% sequence identity and >90% match length
-awk '$3 > 30 && $4 > 0.9 * length($1)' blast_results.txt > filtered_hits.txt
+awk '$3 > 30 && $4 > (0.9 * $5)' blast_results.txt > $output_file
 
-match_count=$(wc -l < filtered_hits.txt)
+match_count=$(wc -l < $output_file)
 echo "Number of matches identified: $match_count"  
 
-# Save the filtered matches to the specified output file
-mv filtered_hits.txt "$output_file"  
-
-# Clean up intermediate files
-rm blast_results.txt 
+#rm blast_results.txt 
